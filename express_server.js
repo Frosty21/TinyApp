04650bb1ef22f08ff252ffd478eef60235fcf0d0
Frosty21 <nathan.froese21@gmail.com>
2017-03-22 19:38:46 -0700

    updated so debugg works in vscode w nodemon

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cookieSession = require('cookie-session');
 const methodOverride = require('method-override');
+const dotenv = require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
